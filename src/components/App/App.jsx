7009d9027fa29525790134bacfcc446ebe4e6140
Vadim <hellovados@gmail.com>
2022-02-18 00:16:02 +0300

    fer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,17 +2,17 @@ import { Header } from "../Header/Header";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ListPage } from "../../Pages/ListPage/ListPage";
 import { BookPage } from "../../Pages/BookPage/BookPage";
-import {Footer} from "../Footer/Footer"
+import { Footer } from "../Footer/Footer";
 
 function App() {
   return (
-    <Router basename="/">
+    <Router basename="/books-future">
       <Header />
       <Routes>
-        <Route path="/" exact element={<ListPage />} />
+        <Route path="" exact element={<ListPage />} />
         <Route path="/:id" exact element={<BookPage />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
